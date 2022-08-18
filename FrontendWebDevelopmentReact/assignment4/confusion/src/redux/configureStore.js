ca5709b1fa3731cbc723dba6de5b59297e3015fd
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2022-08-18 16:34:29 -0500

    Combining Reducers

---
+++ FrontendWebDevelopmentReact/assignment4/confusion/src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import { combineReducers, legacy_createStore as createStore} from 'redux'
+import { Dishes } from './dishes'
+import { Leaders } from './leaders'
+import { Promotions } from './promotions'
+import { Comments } from './comments'
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            leaders: Leaders,
+            promotions: Promotions,
+            comments: Comments
+        })
+    )
+    return store 
+}
