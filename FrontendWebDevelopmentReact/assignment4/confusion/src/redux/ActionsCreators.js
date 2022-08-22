8cd321224360d46d0fb92df4862d312ab9f85054
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2022-08-22 14:20:49 -0500

    Redux Thunk with Logger available

--- FrontendWebDevelopmentReact/assignment4/confusion/src/redux/ActionsCreators.js
+++ FrontendWebDevelopmentReact/assignment4/confusion/src/redux/ActionsCreators.js
@@ -1,4 +1,5 @@
 import * as ActionsTypes from './ActionsTypes'
+import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionsTypes.ADD_COMMENT,
@@ -8,4 +9,25 @@ export const addComment = (dishId, rating, author, comment) => ({
         author: author,
         comment: comment
     }
+})
+
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true))
+    setTimeout(() => {
+        dispatch(addDishes(DISHES))
+    }, 2000);
+}
+
+export const dishesLoading = () => ({
+    type:  ActionsTypes.DISHES_LOADING
+})
+
+export const dishesFailed = (errmess) => ({
+    type: ActionsTypes.DISHES_FAILED,
+    payload: errmess
+})
+
+export const addDishes = (dishes) => ({
+    type: ActionsTypes.ADD_DISHES,
+    payload: dishes
 })
