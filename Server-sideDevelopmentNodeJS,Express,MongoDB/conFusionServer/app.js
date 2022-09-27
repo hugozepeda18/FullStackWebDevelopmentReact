ca8d4950d68e26f4791d997b3fcbcf74e6f761d9
Hugo Zepeda <A01228679@exatec.tec.mx>
2022-09-26 20:40:56 -0500

    User Authentication with Passport

--- Server-sideDevelopmentNodeJS,Express,MongoDB/conFusionServer/app.js
+++ Server-sideDevelopmentNodeJS,Express,MongoDB/conFusionServer/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
 var FileStore = require('session-file-store')(session)
+var passport = require('passport')
+var authenticate = require('./authenticate')
 
 const mongoose = require('mongoose');
 
@@ -40,6 +42,8 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -86,22 +90,14 @@ app.use('/users', usersRouter);
 
 function auth (req, res, next) {
   console.log(req.session);
-
-if(!req.session.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+    next()
   }
-}
 }
 
 app.use(auth);
